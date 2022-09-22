8e11e8f794e68f746d9c6e10e064daea0f52471e
Hunter <realisvenator@gmail.com>
2022-09-22 00:59:28 -0500

    quiz logic done and styling is accpetable.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,8 @@
-const QUESTIONS = [["This is the first question",["A","B","C","D"],"B"],["This is the first question 2",["E","F","G","H"],"H"],["This is the first question 3",['true','false'],'false'],["This is the first question 4",[1,10,30,67],30]];
-var usedQuestions = [];
+const QUESTIONS = [["Javascript is an ______ language?",["Object-Oriented","Object-Based","Procedural","None of the Above"],"Object-Oriented"],["Which of the following keywords is used to define a variable?",["var","let","A and B"],"A and B"],["Which of the following methods is used to access HTML elments using Javascript",['getElementbyId()','getElementByClassName()','A and B','None of the Above'],'A and B'],["Which of the following methods can be used to display data using Javascript?",["document.write()","console.log()","window.alert()","all of the above"],"all of the above"],["What function is used to serialize an object into a JSOn string in Javascript",["stringify()","parse()","cover()"],"stringify()"],["What keyword is used to declare an asynchronous function in Javascript?",["async","await","setTimeout"],"async"]];
 var time=100;
 var qCount=0;
-var scoreCount=0;// insert this data in to the webbrowser insert it into the p element wiht localstorage usingjson stringiyf
+var scoreCount=0;
 var timer=null;
-var startButton = document.getElementById('start');
 var scoreboard=[];
 
 
@@ -19,92 +17,94 @@ const INTRO = document.getElementById('intro');
 const SCORE = document.getElementById('score');
 const START_BUTTON = document.getElementById('start');
 const PAIRINIT = document.getElementById('initials')
-const SCOREB = document.getElementById('scoreb');
-const INPUTT = document.getElementById('inputTake');
+const SCORE_ELEMENT = document.getElementById('scoreb');
+const NAMETAKE = document.getElementById('scoring');
 
 //Helper Functions
-function removeAllChildNodes(parn){
+function removeAllChildNodes(parn){//removes all child nodes from target
     while(parn.firstChild){
-        parn.removeChild(parn.firstChild)
+        parn.removeChild(parn.firstChild);
     }
 }
 
-function setVisibility(target,force='block'){
+function setVisibility(target,force='block'){//sets given visiblity to object
     target.style.display = force;
 }
 
-function restartClock(){
+function restartClock(){// restarts the clock
     time=100;
     refreshClock();
 }
 
-function refreshClock(){
+function refreshClock(){// refresh clock and points
     if(time>=0){
         CLOCK.innerText=time;
+        SCORE.innerText="Points: "+scoreCount;
     }
 }
 //display toggling functions
-function displayStart(){
+function displayStart(){//dislays first screen
     setVisibility(mainTarget,"none");
     setVisibility(OUTRO,"none");
     setVisibility(INTRO,"block");
     setVisibility(START_BUTTON,"block");
+    setVisibility(CLOCK,"block");
 }
 
-function displayMain(){
+function displayMain(){//displays main screen with questions and answers
     setVisibility(INTRO,"none");
     setVisibility(OUTRO,"none");
     setVisibility(mainTarget,"block");
     setVisibility(START_BUTTON,"none");
+    removeAllChildNodes(SCORE_ELEMENT);
+    
 }
 
-function displayEnd(){
+function displayEnd(){//displays end screen
     setVisibility(INTRO,"none");
     setVisibility(mainTarget,"none");
     setVisibility(OUTRO,"block");
-    SCORE.innerText="your score was "+scoreCount;
-    //add input box so they can enter their name and add them to the high score list.
+    setVisibility(NAMETAKE,'block');
 }
 
-function displayBoard(){
+function displayBoard(){//displays board also checks storage
+    removeAllChildNodes(SCORE_ELEMENT);
     setVisibility(INTRO,"none");
     setVisibility(mainTarget,"none");
-    // setVisibility(OUTRO,"none");
-    setVisibility(SCOREB,"block")
-    console.log(scoreboard);
-    for(let i of scoreboard){
-        let tempElement = document.createElement('p');
-        tempElement.innerHTML = i;
-        SCOREB.appendChild(tempElement);
+    setVisibility(OUTRO,"none");
+    setVisibility(SCORE_ELEMENT,"block");
+    if(localStorage.getItem('scoreboard')){
+        scoreboard=JSON.parse(localStorage.getItem('scoreboard'));
     }
+    for(let i of scoreboard){
+        let tempElement = document.createElement('li');
+        tempElement.innerHTML = "Person: "+i[0]+" Score: "+i[1];
+        SCORE_ELEMENT.appendChild(tempElement);
+    }
+    setVisibility(NAMETAKE,'none');
     setVisibility(START_BUTTON,"block");
 }
 
 //main fucntions of the quiz.
-
-function getScoreBoard(){
-    
-}
-
-function toggleButton(){
+function resetScore(){
     localStorage.clear();
+    scoreboard=[];
+    displayBoard();
 }
 
-function addBoard(){
+function addBoard(){//adds score to board and saves to local storage
     if(localStorage.getItem('scoreboard')){
         scoreboard=JSON.parse(localStorage.getItem('scoreboard'));
     }
     scoreboard.push([PAIRINIT.value,scoreCount]);
     let temp = JSON.stringify(scoreboard);
     localStorage.setItem('scoreboard',temp);
-    console.log(temp);
     PAIRINIT.value="";
-    setVisibility(INPUTT,'none');
-    console.log(INPUTT);
     displayBoard()
 }
 
-function restart(){
+function restart(){//restarts game and sets somethings back to default
+    restartClock();
     displayMain();
     scoreCount=0
     qCount=0;
@@ -113,11 +113,7 @@ function restart(){
 }
 
 
-function checkAnswer(answer=true,correct=false) {
-    console.log("-----------");
-    console.log("Answer submited: ",answer);
-    console.log("Correct Answer: ",correct);
-    // targetAnswer.setAttribute("id","feedback-fade");
+function checkAnswer(answer=true,correct=false) {//Checks if button clicked matches correct answeer
     setTimeout(function(){
         SELECTIONS.addEventListener('click', (event)=>{
             event.preventDefault();
@@ -127,44 +123,46 @@ function checkAnswer(answer=true,correct=false) {
 
     if(answer==correct){
         FEEDBACK.setAttribute("id","feedback-fade-right");
-        console.log("Corrected");
         FEEDBACK.innerText="Correct";
         scoreCount++;
     }else{
         FEEDBACK.setAttribute("id","feedback-fade-wrong");
         FEEDBACK.innerText="Wrong";
-        console.log("wrong");
-        time-=2;
+        time-=10;
     }
+    refreshClock();
     nextQuestion();
 }
 
-function nextQuestion(){
+function nextQuestion(){//Gets the next question and displays it. If no more questions displays the end
     clearInterval(timer);
-    // restartClock();
     removeAllChildNodes(SELECTIONS);
     qCount++;
     if(qCount<QUESTIONS.length){
         displayQuestion();
     }else{
         displayEnd();
-        CLOCK.style.visibility= "hidden";
     }
 }
 
-function displayQuestion(){
+function displayQuestion(){//Displays question to screen with all of its answers. Also time keeping
     refreshClock();
     QUESTION_TITLE.innerText = QUESTIONS[qCount][0];
     let tempOl = document.createElement('ol')
-    tempOl.setAttribute("id","selection-section-list"); // add the event lisner to the the ol and check to see if its a button 
+    tempOl.setAttribute("id","selection-section-list");
+    tempOl.addEventListener('click',(event)=>{
+        if(event.target.nodeName == 'BUTTON'){
+            checkAnswer(event.target.getAttribute('selection'),QUESTIONS[qCount][2])
+        }
+    });
+    SELECTIONS.appendChild(tempOl);
     for(let answer of QUESTIONS[qCount][1]){
         let tempListItem = document.createElement('li');
         let tempButton = document.createElement('button');
         tempButton.setAttribute("id","selection-section-button");
-
-        tempButton.addEventListener('click',function(){checkAnswer(answer,QUESTIONS[qCount][2])});
+        tempButton.setAttribute('selection',answer);
         tempButton.innerText=answer;
-        SELECTIONS.appendChild(tempOl.appendChild(tempListItem.appendChild(tempButton)));
+        tempOl.appendChild(tempListItem.appendChild(tempButton));
     }
 
     timer=setInterval(function(){
@@ -181,10 +179,8 @@ function displayQuestion(){
 }
 
 
-function quiz(){
+function quiz(){//Dumb and should be removed
     restart();
-    // displayQuestion()
-
 
 }
 
